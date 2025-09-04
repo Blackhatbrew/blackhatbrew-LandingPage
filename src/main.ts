d5c6a2d11ce0b44e5b0a0835e02329f8845629f5
Muhammad Arafat <97059953+v3cn4x00@users.noreply.github.com>
2025-09-04 23:46:01 +0500

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+// import { createApp } from "vue";
+// import App from "./App.vue";
+
+// createApp(App).mount("#app");
+
+
+import "./assets/index.css";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
+
