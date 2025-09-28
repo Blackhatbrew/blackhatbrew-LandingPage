e16d4716d5ddc5433e80c34222e179894392689c
Muhammad Arafat <97059953+v3cn4x00@users.noreply.github.com>
2025-09-28 16:14:54 +0500

    Update Certificate.vue

--- src/components/Certificate.vue
+++ src/components/Certificate.vue
@@ -11,7 +11,7 @@ const certificates = [
 <template>
   <section class="bg-gray-950 text-white py-10 px-6">
     <h2 class="text-2xl md:text-3xl font-bold mb-6 text-center">
-      📜 My Certificates
+      📜 Team Certificates
     </h2>
 
     <!-- Scrollable container -->
