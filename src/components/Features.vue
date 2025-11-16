97e2d5ec7d40cfafc1050b4383bddf07b4c6e35a
Muhammad Arafat <97059953+v3cn4x00@users.noreply.github.com>
2025-11-16 19:53:30 +0500

    Update Features.vue

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -20,7 +20,7 @@ interface FeaturesProps {
 
  const featureList: FeaturesProps[] = [
   {
-    icon: "skullCrossbones",
+    icon: "goal",
     title: "Red Team CTF Labs",
     description:
       "Access your red team training environment anywhere—designed for desktop and mobile devices without compromising performance.",
